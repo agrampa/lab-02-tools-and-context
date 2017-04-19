ac44ebd028fc36bb24ed4cce64a519e629c3c083
Allie Grampa <agrampa@yahoo.com>
2017-04-18 17:33:27 -0700

    call setup complete

--- lab-allie/lib/methods.js
+++ lab-allie/lib/methods.js
@@ -2,6 +2,22 @@
 
 module.exports = exports = {};
 
+exports.basicSandwichOrder = function(name, meat, bread, cheese) {
+  this.name = name;
+  this.meat = meat;
+  this.bread = bread;
+  this.cheese = cheese;
+};
+
+exports.specialtySandwich = function(name, meat, bread, cheese) {
+  exports.basicSandwichOrder.call(this, meat, bread, cheese);
+  this.largeSize = true;
+  this.mayo = true;
+  this.lettuce = true;
+  this.tomato = true;
+};
+
 exports.sandwichOrder = function(name, meat, bread, cheese) {
   return `${name} would like a ${meat} sandwich on ${bread} with ${cheese}.`;
 };
+
