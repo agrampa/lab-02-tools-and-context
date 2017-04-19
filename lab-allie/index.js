b21b658c0e59edc205ce4477950f6f0a6d2c2902
Allie Grampa <agrampa@yahoo.com>
2017-04-18 18:13:50 -0700

    finished basic tests, first attempt, forgot to make this branch at the beginning

--- lab-allie/index.js
+++ lab-allie/index.js
@@ -6,4 +6,4 @@ console.log(order.basicSandwichOrder('Allie', 'turkey', 'rye', 'swiss'));
 
 console.log(order.specialtySandwich('Dave', 'turkey', 'sourdough', 'provolone'));
 
-console.log(order.miniSandwich('Sofia', 'ham', 'cheddar'));
+console.log(order.miniSandwich('Sofia', 'ham', 'white'));
--- lab-allie/test/methods-test.js
+++ lab-allie/test/methods-test.js
@@ -2,14 +2,33 @@
 
 const assert = require('assert');
 
-const methods = require('../lib/methods.js');
+const order = require('../lib/methods.js');
 
 describe('methods.js', function() {
   describe('sandwichOrder()', function() {
-
+    it('should return a string with the customer order', function() {
+      assert.equal(order.basicSandwichOrder('Allie', 'turkey', 'rye', 'swiss'), 'Allie would like a turkey sandwich on rye with swiss.');
+    });
+    
+    it('should return a string with the customer specialty order', function() {
+      assert.equal(order.specialtySandwich('Dave', 'turkey', 'sourdough', 'provolone'), 'Dave would like a turkey sandwich on sourdough with provolone.');
+    });
+    
+    it('should return a string with the customer mini order', function() {
+      assert.equal(order.miniSandwich('Sofia', 'ham', 'white'), 'Sofia would like a ham sandwich on white.');
+    });
+    //   NOT SURE WHY THIS ISN'T WORKING, COMES UP AS 'PENDING'
+    // it('should be a string for the basic order'), function() {
+    //   assert.equal(typeof order.basicSandwichOrder(), 'string');
+    // };
+    
+    it('should be a string for the speciality order', function() {
+      assert.equal(typeof order.specialtySandwich(), 'string');
+    });
+    
+    it('should be a string for the mini order', function() {
+      assert.equal(typeof order.miniSandwich(), 'string');
+    });
   });
 });
 
-// it('should return a string with the customer order', function() {
-//   assert.equal(order.sandwichOrder('name', 'meat', 'bread', 'cheese'), `${name} would like a ${meat} sandwich on ${bread} with ${cheese}.`);
-// });
